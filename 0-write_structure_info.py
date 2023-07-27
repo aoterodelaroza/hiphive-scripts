5da555a2175a5a47ad5c5d428b581da4803f0cfc
Alberto Otero de la Roza <aoterodelaroza@gmail.com>
2023-07-27 10:09:55 +0200

    added espresso to script 0

--- 0-write_structure_info.py
+++ 0-write_structure_info.py
@@ -7,9 +7,9 @@
 
 ## input block ##
 prefix="blah" ## prefix for the generated files
-eq_structure="POSCAR" ## the equilibrium structure
+eq_structure="mgo.scf.in" ## the equilibrium structure
 ncell = (3,3,3) ## the supercell size
-calculator = "vasp" ## program used for the calculations, case insensitive (VASP)
+calculator = "espresso-in" ## program used for the calculations, case insensitive (vasp,espresso-in)
 #################
 
 import pickle
@@ -18,11 +18,18 @@ import ase.io
 import ase.build
 import phonopy
 
+# process the calculator
+calculator = calculator.lower()
+if (calculator == "espresso-in"):
+    phcalc = "qe"
+else:
+    phcalc = calculator
+
 # reference cell
 cell = ase.io.read(eq_structure)
 
 # supercell: make one phonopy and VASP like
-ph = phonopy.load(unitcell_filename=eq_structure,supercell_matrix=list(ncell))
+ph = phonopy.load(unitcell_filename=eq_structure,supercell_matrix=list(ncell),calculator=phcalc)
 ph = ph.supercell
 scel = ase.Atoms(symbols=ph.symbols,scaled_positions=ph.scaled_positions,cell=ph.cell,pbc=[1,1,1])
 
