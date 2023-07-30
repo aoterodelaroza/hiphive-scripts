3a1de5b6bede1140d13c15ec58e9b3eee9d392e7
Alberto Otero de la Roza <aoterodelaroza@gmail.com>
2023-07-30 02:14:55 +0200

    fix error and improve 1c script: lr forces

--- 1c-calculate_lr_forces.py
+++ 1c-calculate_lr_forces.py
@@ -10,9 +10,8 @@ born_file="BORN" ## file containing the Born charges in VASP format
 
 import pickle
 import numpy as np
-from phonopy import Phonopy
+from phonopy import load
 from phonopy.structure.atoms import PhonopyAtoms
-from phonopy.file_IO import parse_BORN
 
 # load the info file
 with open(prefix + ".info","rb") as f:
@@ -21,15 +20,17 @@ with open(prefix + ".info","rb") as f:
 atoms_phonopy = PhonopyAtoms(symbols=cell.get_chemical_symbols(),
                              scaled_positions=cell.get_scaled_positions(),
                              cell=cell.cell)
-ph = Phonopy(atoms_phonopy, supercell_matrix=ncell*np.eye(3),
-             primitive_matrix=None,calculator=phcalc)
+satoms_phonopy = PhonopyAtoms(symbols=scel.get_chemical_symbols(),
+                             scaled_positions=scel.get_scaled_positions(),
+                             cell=scel.cell)
+ph = load(unitcell=atoms_phonopy, supercell=satoms_phonopy,
+          supercell_matrix=ncell*np.eye(3),
+          born_filename='BORN')
 
-ph.nac_params = parse_BORN(ph.primitive, filename=born_file)
-ph.set_force_constants(np.zeros((len(atoms_phonopy), len(atoms_phonopy), 3, 3)))
+ph.set_force_constants(np.zeros((len(scel), len(scel), 3, 3)))
 dynmat = ph.get_dynamical_matrix()
 dynmat.make_Gonze_nac_dataset()
 fc2_LR = -dynmat.get_Gonze_nac_dataset()[0]
 
 with open(prefix + ".fc2_lr","wb") as f:
     pickle.dump(fc2_LR,f)
-
