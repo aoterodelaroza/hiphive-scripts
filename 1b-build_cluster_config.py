5a63e392ab42b6d316e09d93039cda2c3cc8bba5
Alberto Otero de la Roza <aoterodelaroza@gmail.com>
2024-10-01 16:29:10 +0200

    prevent ase/phonopy error in 1b*py

--- 1b-build_cluster_config.py
+++ 1b-build_cluster_config.py
@@ -11,6 +11,9 @@ cutoffs = [6.32, 3.00] # list of cutoffs [2nd,3rd,...] in angstrom
 import pickle
 from hiphive import ClusterSpace
 import numpy as np
+import ase
+import phonopy
+from phonopy.interface.calculator import get_default_physical_units
 
 ## deactivate deprecation warnings
 import warnings
@@ -22,7 +25,11 @@ with open(prefix + ".info","rb") as f:
     calculator, maximum_cutoff, acoustic_sum_rules, phcalc, ncell, cell, scel, fc_factor, phcel = pickle.load(f)
 
 # build the cluster and save
-cs = ClusterSpace(cell, cutoffs, acoustic_sum_rules=acoustic_sum_rules)
+units = get_default_physical_units(phcalc)
+ph = phcel.supercell
+cell1 = ase.Atoms(cell=ph.cell*units["distance_to_A"], symbols=ph.symbols,
+                  scaled_positions=ph.scaled_positions, pbc=True)
+cs = ClusterSpace(cell1, cutoffs, acoustic_sum_rules=acoustic_sum_rules)
 with open(prefix + ".cs","wb") as f:
     pickle.dump([cutoffs,cs],f)
 
